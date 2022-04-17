b1b6de95c84558af469dab06d73569db3fe20426
Even8 <1016000928@qq.com>
2022-04-17 23:37:56 +0800

    路径引入有问题-有待解决

--- react02_ts/src/App.tsx
+++ react02_ts/src/App.tsx
@@ -1,11 +1,33 @@
 import React, { FC } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Button } from 'antd';
+import './App.less';
+import { Result } from 'antd';
+import { ErrorBoundary } from 'react-error-boundary';
+import styled from 'styled-components';
+import { HEADER_HEIGHT } from '@/config/ui';
+function onError(err:Error) {
+  console.error(err)
+}
+
+let FallbackComponentContainer = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+height: calc(100vh - ${HEADER_HEIGHT}px);
+`
+
+const FallbackComponent = () => {
+  return (
+    <FallbackComponentContainer>
+      <Result status={500} subTitle="哔哔哔... 貌似出了一些问题" />
+    </FallbackComponentContainer>
+  );
+};
 
 const App: FC = () => (
   <div className="App">
-    <Button type="primary">Button</Button>
+    <ErrorBoundary FallbackComponent={FallbackComponent} onError={onError}>
+
+    </ErrorBoundary>
   </div>
 );
 
---
+++ react02_ts/src/config/ui.ts
@@ -0,0 +1,2 @@
+// 顶部占用高度
+export const HEADER_HEIGHT = 48;
